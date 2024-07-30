7bc95fbc7358956eda547eb47ccc48b6721aba98
Gupta, Indramani <indramani.gupta@intl.att.com>
2024-07-31 02:50:22 +0530

    React Learning and Example
    [Learning - 07-31-2024 React Learning and Example]
    
    Co-Authored-By: Indramani Gupta <83708037+indramanigupta@users.noreply.github.com>

---
+++ REACT-LEARNING/reactinterface/craco.config.js
@@ -0,0 +1,21 @@
+/** const autoprefixer = require("autoprefixer");
+const { postcss } = require("tailwindcss"); */
+
+/** module.exports = {
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+}; */
+
+/** @type {import('@craco').Config} */
+
+/** 
+const style = {
+  postcss: {
+    plugins: [require("tailwindcss"), require("autoprefixer")],
+  },
+};
+
+*/
